3407bb499437d1e4462086ec388c50c8b44a256e
carlosbastoslaet <carlosbastoslaet@gmail.com>
2025-11-04 15:17:26 -0300

    novo componente

---
+++ src/test/java/com/store/msshoppingcart/MsShoppingCartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.store.msshoppingcart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsShoppingCartApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
