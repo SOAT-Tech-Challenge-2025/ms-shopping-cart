c239ab639307f33da64c3812b32cd2d129c11d88
KarenL19 <karen-_-19@outlook.com>
2026-01-02 20:55:56 -0300

    ajuste service.tf

--- terraform/k8s_app_service.tf
+++ terraform/k8s_app_service.tf
@@ -1,19 +1,14 @@
 resource "kubernetes_service" "app" {
   metadata {
-    name      = "ms-shopping-cart-service"
+    name = "ms-shopping-cart-service"
     namespace = kubernetes_namespace.tech_challenge.metadata[0].name
   }
 
   spec {
     type = "ClusterIP"
-
     selector = {
-      app = "tech-challenge-app"
+      app = "ms-shopping-cart-app"
     }
-
-    port {
-      port        = 80
-      target_port = 8080
-    }
+    port { port = 80 target_port = 8080 }
   }
 }
