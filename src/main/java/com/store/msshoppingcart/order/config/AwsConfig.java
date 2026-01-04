56a84c018c0914e3502e694665d329c7bdc2e6db
KarenL19 <karen-_-19@outlook.com>
2026-01-03 22:54:23 -0300

    ajuste bean sns

--- src/main/java/com/store/msshoppingcart/order/config/AwsConfig.java
+++ src/main/java/com/store/msshoppingcart/order/config/AwsConfig.java
@@ -7,10 +7,8 @@ import com.amazonaws.services.sns.AmazonSNSClientBuilder;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 
 @Configuration
-@Profile("prod")
 public class AwsConfig {
 
     @Value("${aws.access.key.id}")
